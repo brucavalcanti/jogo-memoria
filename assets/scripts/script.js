18f939676cc617db04cd42e469e472c198361df3
BruCavalcanti <cavalcantibruno1904@gmail.com>
2024-09-11 22:31:02 -0300

    Lógica de randomizar e preencher as cartas implementada

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,43 @@
-
-
-
+/*Variáveis globais*/
 let board = document.querySelector("#boardGame");
+let listaCartas = []
+let cartasTeste = [1,2,3,4,5,6,7]
+let cartasTabuleiro = []
+
+/*Funções para gerar o tabuleiro*/
+
 
 function generateCard(){
   
   let number = prompt("Quantas cartas serão?");
-  while(parseInt(number)%2!==0 || number<0)
+  while(parseInt(number)%2!==0 || number<=0 || number>14)
   {
     number = prompt("Quantas cartas serão?");
   }
+  /*For responsável por preencher as cartas com seus respectivos valores*/
+  for(let i = 0;i<number/2;i++)
+    {
+      cartasTabuleiro.push(cartasTeste[i])
+      cartasTabuleiro.push(cartasTeste[i])
+    }
+  cartasTabuleiro.sort(function(){return 0.5 - Math.random()});  
 
   for(let i=0;i<number;i++){
-    board.innerHTML+=`<div class="card" onClick="turnCard(this)"><p>Frente da Carta</p></div>`
+    board.innerHTML+=`<div class="card" onClick="turnCard(this)"><p>${cartasTabuleiro[i]}</p></div>`
   }
 
 }
-let contador = 0
-let listaCartas = []
+
+/*Funções da Mecânica do jogo*/
 function turnCard(item){
   if(listaCartas.length<2){
     listaCartas.push(item)
     item.classList.add("turn");
-    setTimeout(verificaCartas,2000)
   }
+  if(listaCartas.length==2)
+    {
+      setTimeout(verificaCartas,3000)
+    }
    console.log(listaCartas)
 }
 
@@ -39,3 +53,4 @@ function verificaCartas(){
 function turnBack(item){
   item.classList.remove("turn");
 }
+
